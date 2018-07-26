35cc64b5fdc04e1e0cbe2001af77ab816b5cfff7
Michael Ritter <michael.ritter@tum.de>
2018-07-26 17:22:03 +0200

    added unfill paragraph
    hotkey is C-q

--- init.el
+++ init.el
@@ -687,6 +687,16 @@ _c_ ^+^ _r_ | _d_one      ^ ^  | _o_ops   | _m_: matcher %-5s(ivy--matcher-desc)
   :bind (("C-c C-ö" . next-error))
 )
 ;; ---------- U --------------------------------------------------
+;;; Unfill Paragraph by Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+      "Takes a multi-line paragraph and makes it into a single line of text."
+      (interactive (progn (barf-if-buffer-read-only) '(t)))
+      (let ((fill-column (point-max))
+            ;; This would override `fill-column' if it's an integer.
+            (emacs-lisp-docstring-fill-column t))
+        (fill-paragraph nil region)))
+    ;; Handy key definition
+    (define-key global-map "\C-q" 'unfill-paragraph)
 ;; ---------- V --------------------------------------------------
 ;; ---------- W --------------------------------------------------
 ;; ---------- X --------------------------------------------------
