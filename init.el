0f2eebf579ec0027b807d364a4aafe51e314253d
Michael Ritter <michael.ritter@tum.de>
2018-09-21 10:27:13 +0200

    simplified counsel configuration

--- init.el
+++ init.el
@@ -177,73 +177,23 @@
           ;; Try to complete word as an Emacs Lisp symbol.
           try-complete-lisp-symbol)))
 
-  (use-package counsel :ensure t
-  :bind*
-  (("M-x"     . counsel-M-x)
-   ("C-x C-f" . counsel-find-file)
-   ("C-c f"   . counsel-git)
-   ("C-c s"   . counsel-git-grep)
-   ("C-c /"   . counsel-ag)
-   ("C-c o"   . counsel-find-file-extern)
-   ("C-S-s"   . counsel-ag)
-   ("C-c l"   . counsel-locate))
+(use-package counsel :ensure
+  :commands (counsel-load-theme
+             counsel-bookmark)
+  :bind* (("C-c i" . counsel-imenu)
+          ("C-x C-f" . counsel-find-file)
+          ("C-x C-b" . ivy-switch-buffer)
+          ("C-c C-/" . counsel-rg)
+          ("s-<backspace>" . ivy-switch-buffer)
+          ("M-x" . counsel-M-x))
   :config
-  (setq counsel-find-file-at-point t)
+  (setq counsel-find-file-ignore-regexp
+        (concat "\\(?:\\`[#.]\\)\\|\\(?:[#~]\\'\\)"
+                "\\|\\.x\\'\\|\\.d\\'\\|\\.o\\'"
+                "\\|\\.aux\\'"))
+
   (setq counsel-locate-cmd 'counsel-locate-cmd-mdfind)
-  (setq counsel-find-file-ignore-regexp "\\.DS_Store\\|.git")
-
-  ;; from http://blog.binchen.org/posts/use-ivy-mode-to-search-bash-history.html
-  (defun counsel-yank-bash-history ()
-    "Yank the bash history"
-    (interactive)
-    (let (hist-cmd collection val)
-      (shell-command "history -r")      ; reload history
-      (setq collection
-            (nreverse
-             (split-string (with-temp-buffer (insert-file-contents (file-truename "~/.bash_history"))
-                                             (buffer-string))
-                           "\n"
-                           t)))
-      (when (and collection (> (length collection) 0)
-                 (setq val (if (= 1 (length collection)) (car collection)
-                             (ivy-read (format "Bash history:") collection))))
-        (insert val)
-        (message "%s => kill-ring" val))))
-
-  ;; TODO make the function respects reverse order of file
-  (defun counsel-yank-zsh-history ()
-    "Yank the zsh history"
-    (interactive)
-    (let (hist-cmd collection val)
-      (shell-command "history -r")      ; reload history
-      (setq collection
-            (nreverse
-             (split-string (with-temp-buffer (insert-file-contents (file-truename "~/.zhistory"))
-                                             (buffer-string))
-                           "\n"
-                           t)))
-      (setq collection (mapcar (lambda (it) (replace-regexp-in-string ".*;" "" it)) collection))
-      (when (and collection (> (length collection) 0)
-                 (setq val (if (= 1 (length collection)) (car collection)
-                             (ivy-read (format "Zsh history:") collection :re-builder #'ivy--regex-ignore-order))))
-        (kill-new val)
-        (insert val)
-        (message "%s => kill-ring" val))))
-
-  (defun counsel-package-install ()
-    (interactive)
-    (ivy-read "Install package: "
-              (delq nil
-                    (mapcar (lambda (elt)
-                              (unless (package-installed-p (car elt))
-                                (symbol-name (car elt))))
-                            package-archive-contents))
-              :action (lambda (x)
-                        (package-install (intern x)))
-              :caller 'counsel-package-install))
-  (ivy-set-actions
-   'counsel-find-file
-   '(("o" (lambda (x) (counsel-find-file-extern x)) "open extern"))))
+  (setq counsel-find-file-at-point t))
 
 (use-package counsel-osx-app :ensure t
   :commands counsel-osx-app
@@ -400,9 +350,11 @@ _p_: prev    _r_: reverse
     "C-n" 'ido-next-match
     "C-p" 'ido-prev-match))
 
-(use-package ivy
+(use-package ivy :ensure t
   :quelpa (ivy :fetcher github :repo "abo-abo/swiper")
   :diminish (ivy-mode . "")
+  :bind
+  (:map ivy-mode-map)
   :commands (ivy-switch-buffer
              ivy-switch-buffer-other-window)
   :config
